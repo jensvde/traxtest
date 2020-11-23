44f84a9a6999c281afb39cdd3a24666c6fa7969a
jensvde <jens.vandeneynde@telenet.be>
2020-11-23 01:46:17 +0100

    q

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ SQL_USER_PASS=Admin@2020
 GITLINK=https://github.com/jensvde/traxtest.git
 GITPREFIX=traxtest
 
-sudo apt install -y apache2 mysql-server git nano php libapache2-mod-php php-mysql
+sudo apt install -y expect apache2 mysql-server git nano php libapache2-mod-php php-mysql
 
 sudo rm /var/www/html/*.*
 git clone $GITLINK /var/www/html/
@@ -15,7 +15,13 @@ sudo mv /var/www/html/$GITPREFIX/*.* /var/www/html/
 
 #Create MySQL users and database
 sudo mysql -e "CREATE DATABASE logs;"
-sudo mysql -e "CREATE TABLE logentries (id INT, username VARCHAR2(300), password VARCHAR2(300), ip_address VARCHAR2(300), datetime));"
+mysql -e "CREATE TABLE logentries (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+username VARCHAR(300) NOT NULL,
+password VARCHAR(300) NOT NULL,
+ip_address VARCHAR(300),
+date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)"
 sudo mysql -e "CREATE USER '$SQL_USER'@'localhost' IDENTIFIED WITH mysql_native_password BY '$SQL_USER_PASS';"
 sudo mysql -e "GRANT ALL ON *.logs TO '$SQL_USER'@'localhost';"
 sudo mysql -e "FLUSH PRIVILEGES;"
