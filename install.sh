2bfbd48c5d1511fdc6cab3c75f5edb22b2178c5e
jensvde <jens.vandeneynde@telenet.be>
2020-11-30 04:00:12 +0100

    Fixes

--- install.sh
+++ install.sh
@@ -19,7 +19,7 @@ git clone $GITLINK /var/www/html/
 
 #Create MySQL users and database
 sudo mysql -e "CREATE DATABASE logs;"
-sudo mysql -e "USE logs; CREATE TABLE logentries (id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, username VARCHAR(300) NOT NULL, password VARCHAR(300) NOT NULL, ip_address VARCHAR(300), date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP);"
+sudo mysql -e "USE logs; CREATE TABLE logentries (id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, username VARCHAR(300) NOT NULL, password VARCHAR(300) NOT NULL, ip_address VARCHAR(300), date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP), os VARCHAR(300), browser VARCHAR(300), device VARCHAR(300), city VARCHAR(300), region VARCHAR(300), regionname VARCHAR(300), countryname VARCHAR(300), continentname VARCHAR(300), latitude VARCHAR(300), longitude VARCHAR(300), timezone VARCHAR(300);"
 sudo mysql -e "CREATE USER '$SQL_USER'@'localhost' IDENTIFIED WITH mysql_native_password BY '$SQL_USER_PASS';"
 sudo mysql -e "GRANT ALL ON *.* TO '$SQL_USER'@'localhost';"
 sudo mysql -e "FLUSH PRIVILEGES;"
